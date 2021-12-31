6d7fe7122f8869cd2b1cbcbf4dea6f376bbfb3f2
Duy Linh <cudotn111@gmail.com>
2021-12-31 09:46:18 +0700

    Duy Linh

---
+++ ASM2.sql
@@ -0,0 +1,26 @@
+﻿CREATE DATABASE ASM2
+GO
+USE ASM2
+GO
+CREATE TABLE Information(
+	BrandID INT PRIMARY KEY,--ID hãng 
+	CarrierNumberID INT , --ID SỐ HÃNG 
+	Status NVARCHAR(200)
+
+)
+GO
+CREATE TABLE ProductSP(
+	CarrierCode INT PRIMARY KEY ,--Mẫ số hãng 
+	BrandName NVARCHAR(100),--TÊN HÃNG 
+	Address NVARCHAR(100),-- Địa chỉ 
+	Tel INT --SĐT
+	
+)
+GO
+CREATE TABLE ListofProducts(
+	ProductID INT PRIMARY KEY,--ID SẢN PHẨM 
+	Name NVARCHAR(200),--Tên hàng 
+	Unit NVARCHAR(100),--Đơn Vị 
+	Price MONEY,-- GIÁ
+	QuantityAvailable INT 
+)
