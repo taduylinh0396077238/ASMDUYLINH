11a7eb8d496661a5411c618fa32eb85a7f72b05c
Duy Linh <cudotn111@gmail.com>
2022-01-02 09:50:26 +0700

    Duy Linh

--- ASM2.sql
+++ ASM2.sql
@@ -1,26 +1,147 @@
-﻿CREATE DATABASE ASM2
+﻿CREATE DATABASE Asm2
 GO
-USE ASM2
+USE Asm2
+GO 
+CREATE TABLE SpHang(
+	MaHang INT NOT NULL  PRIMARY KEY,
+	TenHang NVARCHAR(200),
+	DiaChi NVARCHAR(200),
+	Phone  CHAR(20) CHECK(ISNUMERIC(Phone)=1)
+)
 GO
-CREATE TABLE Information(
-	BrandID INT PRIMARY KEY,--ID hãng 
-	CarrierNumberID INT , --ID SỐ HÃNG 
-	Status NVARCHAR(200)
+CREATE TABLE MaLoaiSP(
+	MaLoai NVARCHAR(200)  PRIMARY KEY,
+	LoaiSP NVARCHAR(100) 
+)
+GO
+CREATE TABLE DanhSachSP(
+	MaSP NVARCHAR(100) PRIMARY KEY,
+	TenSP NVARCHAR(300),
+	MaHang INT FOREIGN KEY REFERENCES dbo.SpHang(MaHang),
+	MoTa NVARCHAR(100),
+	DonVi NVARCHAR(200),
+	Gia INT,
+	SoLuong INT,
+	MaLoai NVARCHAR(200) FOREIGN KEY REFERENCES dbo.MaLoaiSP(MaLoai)
+)
 
+
+INSERT INTO dbo.SpHang
+(
+    MaHang,
+    TenHang,
+    DiaChi,
+    Phone
 )
-GO
-CREATE TABLE ProductSP(
-	CarrierCode INT PRIMARY KEY ,--Mẫ số hãng 
-	BrandName NVARCHAR(100),--TÊN HÃNG 
-	Address NVARCHAR(100),-- Địa chỉ 
-	Tel INT --SĐT
-	
+VALUES
+(   123,    -- MaHang - int
+    N'LapTop', -- TenHang - nvarchar(200)
+    N'USA ', -- DiaChi - nvarchar(200)
+    '0983232'  -- Phone - int
+    ),
+(   234,    -- MaHang - int
+    N'APPLE', -- TenHang - nvarchar(200)
+    N'USA ', -- DiaChi - nvarchar(200)
+    '0983233'  -- Phone - int
+    ),
+(   456,    -- MaHang - int
+    N'SAMSUNG', -- TenHang - nvarchar(200)
+    N'USA ', -- DiaChi - nvarchar(200)
+    '0983234'  -- Phone - int
+    )
+
+
+INSERT INTO dbo.MaLoaiSP
+(
+    MaLoai,
+    LoaiSP
 )
-GO
-CREATE TABLE ListofProducts(
-	ProductID INT PRIMARY KEY,--ID SẢN PHẨM 
-	Name NVARCHAR(200),--Tên hàng 
-	Unit NVARCHAR(100),--Đơn Vị 
-	Price MONEY,-- GIÁ
-	QuantityAvailable INT 
+VALUES
+(   'X01', -- MaLoai - nvarchar(200)
+    N'APPle'  -- LoaiSP - nvarchar(100)
+    ),
+(   'X02', -- MaLoai - nvarchar(200)
+    N'Điện THoại '  -- LoaiSP - nvarchar(100)
+    ),
+(   'X03', -- MaLoai - nvarchar(200)
+    N'LapTop'  -- LoaiSP - nvarchar(100)
+    )
+INSERT INTO dbo.DanhSachSP
+(
+    MaSP,
+    TenSP,
+    MaHang,
+    MoTa,
+    DonVi,
+    Gia,
+    SoLuong,
+	MaLoai
 )
+VALUES
+(   N'01',  -- MaSP - nvarchar(100)
+    N'APPLE', -- TenSP - nvarchar(300)
+    123, -- MaHang - int
+    N'NEW', -- MoTa - nvarchar(100)
+    N'CHIẾC ', -- DonVi - nvarchar(200)
+    1000, -- Gia - int
+    10, -- SoLuong - int
+	'X01'  -- MaLoai -
+    ),
+	(   N'02',  -- MaSP - nvarchar(100)
+    N'SAMSUNG', -- TenSP - nvarchar(300)
+    234, -- MaHang - int
+    N'NEW', -- MoTa - nvarchar(100)
+    N'CHIẾC ', -- DonVi - nvarchar(200)
+    200, -- Gia - int
+    200, -- SoLuong - int
+	'X02'  -- MaLoai -
+    ),
+	(   N'03',  -- MaSP - nvarchar(100)
+    N'LAPTOP', -- TenSP - nvarchar(300)
+    456, -- MaHang - int
+    N'NEW', -- MoTa - nvarchar(100)
+    N'CHIẾC ', -- DonVi - nvarchar(200)
+    10, -- Gia - int
+    100, -- SoLuong - int
+	'X03'  -- MaLoai -
+    )
+
+SELECT*FROM dbo.SpHang
+SELECT*FROM dbo.DanhSachSP
+SELECT *FROM dbo.MaLoaiSP
+--Truy Vẫn Đề
+--4A Hiển Thị Các hãng sản xuất 
+SELECT*FROM dbo.SpHang
+--4B Hiển Thị Tất Cả Các SP
+SELECT * FROM dbo.DanhSachSP
+--5A Liệt kê danh sách hãng theo thứ thự ngược với alphabet của tên.
+SELECT*FROM dbo.SpHang ORDER BY TenHang 
+--5B Liệt kê danh sách sản phẩm của cửa hàng theo thứ thự giá giảm dần.
+SELECT*FROM dbo.DanhSachSP ORDER BY Gia DESC
+--5c Hiển thị thông tin của hãng Asus.
+SELECT*FROM dbo.SpHang 
+WHERE TenHang LIKE 'APPLE%'
+--5D Liệt kê danh sách sản phẩm còn ít hơn 11 chiếc trong kho
+SELECT *FROM dbo.DanhSachSP
+WHERE SoLuong < 11
+--5e Liệt kê danh sách sản phẩm của hãng Asus
+SELECT*FROM dbo.DanhSachSP
+WHERE TenSP LIKE 'APPLE%'
+--6A Số hãng sản phẩm mà cửa hàng có.
+SELECT COUNT(MaHang) AS'SoHang'  FROM dbo.SpHang
+
+--6b Số mặt hàng mà cửa hàng bán.
+SELECT COUNT(MaSP) FROM dbo.DanhSachSP
+--6c Tổng số loại sản phẩm của mỗi hãng có trong cửa hàng.
+SELECT MaLoai,COUNT(MaSP) AS'Tổng' FROM dbo.DanhSachSP
+GROUP BY MaLoai
+--6d Tổng số đầu sản phẩm của toàn cửa hàng
+SELECT SUM(SoLuong) FROM dbo.DanhSachSP
+--7a Viết câu lệnh để thay đổi trường giá tiền của từng mặt hàng là dương(>0).
+ALTER TABLE dbo.DanhSachSP
+	ADD CONSTRAINT
+CHECK_GIA CHECK(Gia>0)
+--7B Viết câu lệnh để thay đổi số điện thoại phải bắt đầu bằng 0.
+ALTER TABLE dbo.SpHang
+	ADD CHECK(LEFT(Phone,1)='0')
+--7c Viết các câu lệnh để xác định các khóa ngoại và khóa chính của các bảng.
