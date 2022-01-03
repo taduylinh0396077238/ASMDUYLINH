d262a4c7be8ec19b296443ec8c56f09741137dc8
Duy Linh <cudotn111@gmail.com>
2022-01-03 21:33:36 +0700

    Duy Linh

---
+++ AS5.sql
@@ -0,0 +1,98 @@
+﻿CREATE DATABASE ASM5
+GO 
+USE ASM5
+GO
+CREATE TABLE SĐT(
+	TenIDDB INT PRIMARY KEY ,
+	Phone CHAR(20)
+)
+
+GO 
+CREATE TABLE ThongTinkh (
+	TenIDDB INT FOREIGN KEY REFERENCES dbo.SĐT(TenIDDB),
+	Ten NVARCHAR(200),
+	DiaChi NVARCHAR(300),
+	NgaySinh DATE
+)
+INSERT INTO dbo.SĐT
+(
+    TenIDDB,
+    Phone
+)
+VALUES
+(   0,   -- TenIDDB - int
+    '0396077238' -- Phone - int
+    ),
+(   1,   -- TenIDDB - int
+    '0987654345' -- Phone - int
+    ),
+(   2,   -- TenIDDB - int
+    '0977603809' -- Phone - int
+    )
+INSERT INTO dbo.ThongTinkh
+(
+    TenIDDB,
+    Ten,
+    DiaChi,
+    NgaySinh
+)
+VALUES
+(   0, -- TenIDDB - int
+    N'Nguyễn Văn A ', -- Ten - nvarchar(200)
+    N'Thái Nguyên ', -- DiaChi - nvarchar(300)
+    '20030715'  -- NgaySinh - date
+    ),
+(   1, -- TenIDDB - int
+    N'Nguyễn Văn B ', -- Ten - nvarchar(200)
+    N'Thái Bình ', -- DiaChi - nvarchar(300)
+    '20030109'  -- NgaySinh - date
+    ),
+(   2, -- TenIDDB - int
+    N'Nguyễn Văn C ', -- Ten - nvarchar(200)
+    N'Ninh Bình ', -- DiaChi - nvarchar(300)
+    '19990103'  -- NgaySinh - date
+    )
+--4a Liệt kê danh sách những người trong danh bạ
+SELECT*FROM dbo.ThongTinkh
+--4b Liệt kê danh sách số điện thoại có trong danh bạ
+SELECT* FROM dbo.SĐT
+--5a Liệt kê danh sách người trong danh bạ theo thứ thự alphabet.
+SELECT* FROM dbo.ThongTinkh
+ORDER BY Ten
+--5b Liệt kê các số điện thoại của người có thên là Nguyễn Văn A
+SELECT * FROM dbo.SĐT
+WHERE Phone = '0396077238'
+--5c Liệt kê những người có ngày sinh là 15/07/2003
+SELECT * FROM dbo.ThongTinkh
+WHERE NgaySinh = '20030715'
+--6a Tìm số lượng số điện thoại của mỗi người trong danh bạ.
+SELECT *FROM dbo.ThongTinkh
+WHERE TenIDDB = (
+	SELECT TenIDDB FROM dbo.SĐT
+	WHERE Ten = 'Nguyễn Văn A '
+)
+--6b Tìm tổng số người trong danh bạ sinh vào thang 01
+SELECT (MONTH(NgaySinh)) AS'month',COUNT(*) AS NUMBER_OF_BIRTHD
+FROM dbo.ThongTinkh
+WHERE (MONTH(NgaySinh)) = '1'
+GROUP BY (MONTH(NgaySinh))
+--6c Hiển thị toàn bộ thông tin về người, của từng số điện thoại.
+SELECT* FROM dbo.SĐT
+JOIN dbo.ThongTinkh
+ON ThongTinkh.TenIDDB = SĐT.TenIDDB
+--6D Hiển thị toàn bộ thông tin về người, của số điện thoại 123456789.
+SELECT* FROM dbo.SĐT
+JOIN dbo.ThongTinkh
+ON ThongTinkh.TenIDDB = SĐT.TenIDDB
+WHERE Ten = 'Nguyễn Văn B '
+--7A Viết câu lệnh để thay đổi trường ngày sinh là trước ngày hiện tại.
+ALTER TABLE dbo.ThongTinkh
+ADD CONSTRAINT
+CHECK_NGAY CHECK(NgaySinh<= GETDATE())
+--7B  Viết câu lệnh để xác định các trường khóa chính và khóa ngoại của các bảng.
+
+
+
+
+
+s
---
+++ ASM4.sql
@@ -0,0 +1,115 @@
+﻿CREATE DATABASE ASM4
+GO
+USE ASM4
+GO
+CREATE TABLE LoaiSP(
+	MaLoaiSP CHAR(20) PRIMARY KEY,
+	TenLoaiSP NVARCHAR(200),
+	
+
+)
+GO
+CREATE TABLE NguoiPhutrach (
+	MaNPT INT PRIMARY KEY,
+	TenNPT NVARCHAR(200)
+)
+GO
+CREATE TABLE SanPham(
+	MaSSP CHAR(20) PRIMARY KEY,
+	NgaySX DATE,
+	MaLoaiSP CHAR(20) FOREIGN KEY REFERENCES dbo.LoaiSP(MaLoaiSP),
+	MaNPT INT FOREIGN KEY REFERENCES dbo.NguoiPhutrach(MaNPT)
+)
+INSERT INTO dbo.LoaiSP
+(
+    MaLoaiSP,
+    TenLoaiSP
+)
+VALUES
+(   'Z37E',  -- MaLoaiSP - char(20)
+    N'Máy tính cầm tay' -- TenLoaiSP - nvarchar(200)
+    ),
+(   'Z38E',  -- MaLoaiSP - char(20)
+    N'Máy tính ko dây ' -- TenLoaiSP - nvarchar(200)
+    ),
+(   'Z39E',  -- MaLoaiSP - char(20)
+    N'Máy Tính có dây ' -- TenLoaiSP - nvarchar(200)
+    )
+INSERT INTO dbo.NguoiPhutrach
+(
+    MaNPT,
+    TenNPT
+)
+VALUES
+(   1,   -- MaNPT - int
+    N'Tạ Duy Linh ' -- TenNPT - nvarchar(200)
+    ),
+(   2,   -- MaNPT - int
+    N'Vú Viết Qúy ' -- TenNPT - nvarchar(200)
+    )
+INSERT INTO dbo.SanPham
+(
+    MaSSP,
+    NgaySX,
+    MaLoaiSP,
+    MaNPT
+)
+VALUES
+(   '123',   -- MaSSP - char(20)
+    '20220102', -- NgaySX - date
+    'Z37E', -- MaLoaiSP - char(20)
+    1  -- MaNPT - int
+    ),
+(   '345',   -- MaSSP - char(20)
+    '20211209', -- NgaySX - date
+    'Z38E', -- MaLoaiSP - char(20)
+    2 -- MaNPT - int
+    ),
+(   '678',   -- MaSSP - char(20)
+    '20211112', -- NgaySX - date
+    'Z39E', -- MaLoaiSP - char(20)
+    1 -- MaNPT - int
+    )
+--4a Liệt kê danh sách loại sản phẩm của công ty.
+SELECT *FROM dbo.DanhSachSP
+--4B Liệt kê danh sách sản phẩm của công ty.
+SELECT*FROM dbo.SanPham
+--4C Liệt kê danh sách người chịu trách nhiệm của công ty.
+SELECT*FROM dbo.NguoiPhutrach
+--5a  Liệt kê danh sách loại sản phẩm của công ty theo thứ tự tăng dần của tên
+SELECT*FROM dbo.LoaiSP 
+ORDER BY TenLoaiSP ASC 
+--5b Liệt kê danh sách người chịu trách nhiệm của công ty theo thứ tự tăng dần của tên.
+SELECT*FROM dbo.NguoiPhutrach
+ORDER BY  TenNPT ASC
+--5C Liệt kê các sản phẩm của loại sản phẩm có mã số là Z37E.
+SELECT*FROM dbo.LoaiSP 
+WHERE MaLoaiSP = 'Z37E'
+--5D Liệt kê các sản phẩm Nguyễn Văn An chịu trách nhiệm theo thứ tự giảm đần của mã.
+SELECT *FROM dbo.SanPham
+WHERE MaNPT = (
+	SELECT MaNPT FROM dbo.NguoiPhutrach
+	WHERE TenNPT = N'Tạ Duy Linh'
+)
+--6A Số sản phẩm của từng loại sản phẩm.
+SELECT MaLoaiSP,COUNT(MaSSP) AS 'Số Sp' FROM dbo.SanPham
+GROUP BY MaLoaiSP
+--6c Hiển thị toàn bộ thông tin về sản phẩm và loại sản phẩm.
+SELECT *FROM dbo.SanPham
+INNER JOIN dbo.LoaiSP
+ON LoaiSP.MaLoaiSP = SanPham.MaLoaiSP
+--6d Hiển thị toàn bộ thông tin về người chịu trách nhiêm, loại sản phẩm và sản phẩm.
+SELECT * FROM dbo.NguoiPhutrach
+JOIN dbo.SanPham
+ON SanPham.MaNPT = NguoiPhutrach.MaNPT
+JOIN dbo.LoaiSP
+ON LoaiSP.MaLoaiSP = SanPham.MaLoaiSP
+
+--7A Viết câu lệnh để thay đổi trường ngày sản xuất là trước hoặc bằng ngày hiện tại.
+ALTER TABLE dbo.SanPham 
+ADD CONSTRAINT
+CHECK_Ngay CHECK(NgaySX<= GETDATE())
+--7b Viết câu lệnh để xác định các trường khóa chính và khóa ngoại của các bảng.
+--7c Viết câu lệnh để thêm trường phiên bản của sản phẩm.
+ALTER TABLE dbo.SanPham
+ADD PhienBan INT 
